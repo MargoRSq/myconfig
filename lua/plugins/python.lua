aa46099e76936288bf5da7565739d815ec4c6f0f
margosq <slava1kvartovkin@gmail.com>
2023-10-28 00:08:11 +0300

    keymaps: venv-selector

--- lua/plugins/python.lua
+++ lua/plugins/python.lua
@@ -7,12 +7,26 @@ return {
 	event = "VeryLazy", -- Optional: needed only if you want to type `:VenvSelect` without a keymapping
 	keys = {
 		{
-			-- Keymap to open VenvSelector to pick a venv.
 			"<leader>vs",
-			"<cmd>:VenvSelect<cr>",
-			-- Keymap to retrieve the venv from a cache (the one previously used for the same project directory).
+			function()
+				require("venv-selector").open()
+			end,
+			desc = "Select venv",
+		},
+		{
 			"<leader>vc",
-			"<cmd>:VenvSelectCached<cr>",
+			function()
+				require("venv-selector").retrieve_from_cache()
+			end,
+			desc = "Open last venv",
 		},
 	},
+	-- 	{
+	-- 		-- Keymap to open VenvSelector to pick a venv.
+	-- 		"<leader>vs",
+	-- 		"<cmd>:VenvSelect<cr>",
+	-- 		-- Keymap to retrieve the venv from a cache (the one previously used for the same project directory).
+	-- 		"<leader>vc",
+	-- 		"<cmd>:VenvSelectCached<cr>",
+	-- 	},
 }
